3ecc2e1a07698d8dedd20d857d08c34005f36943
Thoretton Edwin <edwin.thoretton@wizbii.com>
2017-09-09 18:00:58 +0200

    add Material theme

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = new ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.yellow,
+    primaryColor: const Color(0xffffeb3b),
+    primaryColorBrightness: Brightness.light,
+    accentColor: const Color(0xff616161),
+    accentColorBrightness: Brightness.light
+);
